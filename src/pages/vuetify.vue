204925bb934ab2c02280d5318728f9f4c6ee454e
Dmitry Mamyrev <dmitry.mamyrev@epicmax.co>
2025-02-01 12:53:03 +0300

    vuetify component used for virtual scroll

---
+++ src/pages/vuetify.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1 class="text-center">Vuetify Virtual List</h1>
+  <vuetify-list class="mt-4" />
+</template>
+
+<script setup>
+import VuetifyList from '@/components/VuetifyList.vue'
+</script>
