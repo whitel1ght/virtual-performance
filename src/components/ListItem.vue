e5e391e645e03e0e05e6586dd5abb405ca05f38d
Dmitry Mamyrev <dmitry.mamyrev@epicmax.co>
2025-02-01 12:40:50 +0300

    list item component markup

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -1,28 +1,114 @@
 <template>
   <div class="list-item">
-    {{ item.name }}
+    <div class="list-item__checkbox">
+      <v-checkbox-btn density="compact" />
+    </div>
+
+    <div class="list-item__content">
+      <div>{{ item.name }}</div>
+      <div>{{ item.description }}</div>
+
+      <div class="list-item__content-actions">
+        <v-tooltip
+          location="top"
+          v-for="action in actions"
+          :key="action.id"
+        >
+          <template #activator="{ props }">
+            <v-btn
+              density="compact"
+              variant="text"
+              :key="action.id"
+              :icon="action.icon"
+              @click="() => $emit(action.event, item)"
+              v-bind="props"
+              class="list-item__content-actions__btn"
+            />
+          </template>
+          <span>{{ action.name }}</span>
+        </v-tooltip>
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script setup>
-
 const props = defineProps({
   item: {
-    type: Array,
+    type: Object,
     default: () => []
   }
 })
 
 const actions = [
-  { id: 'copy', title: "Copy",  }
+  {
+    id: 1,
+    event: 'refresh',
+    name: 'Refresh',
+    icon: 'mdi-refresh'
+  },
+  {
+    id: 2,
+    event: 'edit',
+    name: 'Edit',
+    icon: 'mdi-pencil'
+  },
+  {
+    id: 3,
+    event: 'delete',
+    name: 'Delete',
+    icon: 'mdi-delete'
+  },
+  {
+    id: 4,
+    event: 'copy',
+    name: 'Copy',
+    icon: 'mdi-content-copy'
+  }
 ]
 </script>
 
-<style>
+<style lang="scss">
 .list-item {
+  display: flex;
   height: 100px;
   background-color: #fff;
   border-bottom: 1px solid #e0e0e0;
-  padding: 20px;
+
+  &__checkbox {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0 10px;
+  }
+
+  &__content {
+    padding: 10px;
+    display: flex;
+    flex-grow: 1;
+    flex-direction: column;
+    justify-content: space-between;
+
+    &-actions {
+      display: flex;
+      justify-content: flex-end;
+      gap: 5px;
+      width: 100%;
+      height: 30px;
+
+      .v-btn {
+        display: none;
+      }
+    }
+  }
+}
+
+.list-item:hover {
+  background-color: #f5f5f5;
+
+  .list-item__content-actions .v-btn {
+    display: block;
+  }
 }
 </style>
---
+++ src/components/SimpleListItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="list-item">
+    {{ item.name }}
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  item: {
+    type: Array,
+    default: () => []
+  }
+})
+</script>
+
+<style lang="css">
+.list-item {
+  height: 100px;
+  background-color: #fff;
+  border-bottom: 1px solid #e0e0e0;
+  padding: 20px;
+}
+</style>
