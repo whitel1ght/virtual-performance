67780309e3444ea3b41218fad09deed8dbefdf1f
Dmitry Mamyrev <dmitry.mamyrev@epicmax.co>
2025-02-05 17:47:17 +0300

    default example added

---
+++ src/pages/default-example.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1 class="text-center"> Default Virtual List Example for Any Framework</h1>
+
+  <v-virtual-scroll
+    :items="items"
+    :height="600"
+  >
+    <template #default="{ item }">
+      {{ item.name }}
+    </template>
+  </v-virtual-scroll>
+</template>
+
+<script setup>
+
+const items = Array.from({ length: 1000 }, (_, i) => ({
+  id: i,
+  name: `Item ${i}`
+}))
+</script>
