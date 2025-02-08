62b8855d2f2b277aacba1ac5d244b9d495a97368
Dmitry Mamyrev <dmitry.mamyrev@epicmax.co>
2025-02-08 12:26:04 +0300

    usual list added

---
+++ src/components/UsualList.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul class="usual-list">
+    <li
+      v-for="item in items"
+      :key="item.id"
+      class="usual-list__item"
+    >
+      <list-item :item="item" />
+    </li>
+  </ul>
+</template>
+
+<script setup>
+import ListItem from '@/components/ListItem.vue'
+
+const items = Array.from({ length: 100 }, (_, i) => ({
+  id: i,
+  name: `Item ${i}`,
+  description: `Description for item ${i}`
+}))
+</script>
+
+<style lang="scss">
+.usual-list {
+  height: 600px;
+  overflow-y: auto;
+}
+</style>
---
+++ src/pages/usual-list.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1 class="text-center">Usual List</h1>
+  <usual-list class="mt-4" />
+</template>
+
+<script setup>
+import UsualList from '@/components/UsualList.vue'
+</script>
+
+<style>
+</style>
